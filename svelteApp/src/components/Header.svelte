979d6db0f710afe7311c765bd24eab701bdea85d
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2021-07-01 11:31:36 +0530

    header and footer complete

---
+++ svelteApp/src/components/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+
+</script>
+
+<header>
+    <h1>Live TV</h1>
+    <div class="outs">
+        <h1>Signin</h1>
+        <h1>Signout</h1>
+    </div>
+</header>
+
+<style>
+    h1{
+        margin:15px;
+        cursor: pointer;
+    }
+    header{
+        background-color: white;
+        padding:0px;
+        display: flex;
+        justify-content: space-between;
+        margin-bottom:20px;
+    }
+    .outs{
+        display: flex;
+    }
+</style>
